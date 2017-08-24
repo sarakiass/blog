1d587c971df537dd2ec56f20d981d068491f2c95
Sara KIASS <kiass.sara@gmail.com>
2017-08-24 09:48:48 +0100

    rédaction du code html

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="UTF-8"/>
+       <title>Mon blog!</title>
+ </head>
+<body>
+ <h1>Dernies billets du blog:</h1>
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+ </body>
+ </html>
